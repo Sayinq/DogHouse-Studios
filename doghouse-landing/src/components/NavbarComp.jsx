8dbe47bbbab4671d965bc612482e57e3e3528219
MylesGit <37464485+Sayinq@users.noreply.github.com>
2023-11-29 15:41:58 -0500

    initial commit of files

---
+++ doghouse-landing/src/components/NavbarComp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavbarComp = () => {
+    return (
+        <nav id="navbar" className="">
+            <div id="navbar-wrapper" className="">
+                
+            </div>
+        </nav>
+    )
+}
+
+export default NavbarComp
---
+++ doghouse-landing/src/page/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <main>
+            <section className="w-screen h-screen bg-[#1A1A1A]">
+                <h2 className="text-white">Texty text</h2>
+            </section>
+        </main>
+    )
+}
+
+export default Home
